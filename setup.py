4abfe9f76760b4582bc77365ec5d18f12696d7ac
Francesco Spinnato <fspinna@gmail.com>
2022-02-25 17:30:08 +0100

    no find_packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='autoencoders',
     version='0.0.1',
-    packages=find_packages(),  # ['autoencoders'],
+    packages=['autoencoders'],  # find_packages(),  # ['autoencoders'],
     url='',
     license='',
     author='Francesco Spinnato',
